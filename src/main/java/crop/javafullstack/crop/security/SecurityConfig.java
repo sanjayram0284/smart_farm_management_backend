8889b35bd293394595a194dbfaef79838c1b9ae6
sanjayram0284 <sanjayram6379@gmail.com>
2026-01-06 13:53:01 +0530

    changed url

--- src/main/java/crop/javafullstack/crop/security/SecurityConfig.java
+++ src/main/java/crop/javafullstack/crop/security/SecurityConfig.java
@@ -53,7 +53,10 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOriginPatterns(List.of(
+        "http://localhost:5173",
+        "https://smart-farm-management.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
